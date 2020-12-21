78fa92cea6e61afc4f534ffd04f32164c6d52c33
sdrahan <serhii.drahan@deposit-solutions.com>
2020-12-21 12:53:33 +0100

    Allow cross-origin and slightly improve request error handling

--- numbarb.go
+++ numbarb.go
@@ -15,13 +15,17 @@ func main() {
 
 func convert(w http.ResponseWriter, req *http.Request) {
 	numbers, ok := req.URL.Query()["number"]
+	w.Header().Set("Access-Control-Allow-Origin", "*")
 
 	if !ok || len(numbers[0]) < 1 {
+		fmt.Fprintf(w, "bad request or missing param")
 		return
 	}
 
 	number, err := strconv.Atoi(numbers[0])
 	if err == nil {
 		fmt.Fprintf(w, converters.ConvertIntegerToWords(int(number)))
+	} else {
+		fmt.Fprintf(w, "couldn't get number")
 	}
 }
